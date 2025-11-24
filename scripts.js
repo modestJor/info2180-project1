4d62b788080c36321f10f5dd100067e2333a6388
Jordan Graham <triplejg112@gmail.com>
2025-11-23 23:17:08 -0500

    Added Final files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+window.onload = function() {
+    const form = this.document.querySelector(".newsletter form");
+    const input = this.document.querySelector(".newsletter input");
+    const message = this.document.querySelector(".message");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const email = input.value.trim();
+        if (email === "") {
+            message.textContent = "Please enter a valid email address.";
+            message.style.color = "red";
+            return;
+        }
+        else {
+            message.textContent = "Thank you! Your email address " + email + " has been added to our mailing list!";
+            message.style.color = "white";
+        }
+
+    });
+};
